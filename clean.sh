8a3d62bd9ce705f00964ab57258a8931ff72fbcf
kiam001 <blablatupfen@gmail.com>
2021-05-21 09:49:09 +0000

    Purge Gitea

--- clean.sh
+++ clean.sh
@@ -7,11 +7,6 @@ source "${my_dir}/${my_txt}"
 # Save devicename for overite (1/2)
 export device_real=$device
 
-if [ -z "${GITEA_TOKEN}" ]; then
-    echo "Please set GITEA_TOKEN before continuing."
-    exit 1
-fi
-
 # Email for git
 git config --global user.email "${GITHUB_EMAIL}"
 git config --global user.name "${GITHUB_USER}"
